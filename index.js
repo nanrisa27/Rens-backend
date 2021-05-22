52fb457ebe102a5ee6322874ca61715db1dec1ed
Minyoi87 <59666333+Minyoi87@users.noreply.github.com>
2021-05-23 00:14:59 +0300

    created backend server and inventory.js file

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+
+const app = express();
+const PORT= 5000;
+
+app.use(bodyParser.json());
+
+app.get('/',(req,res)=>{res.send("hello from homepage")})
+
+app.listen(PORT,()=> console.log(`server running on port:${PORT}`))
